b0f45a20d98fb32afb80dc17d7f4cc4148fef740
Victor Guthrie <guthrievictor@gmail.com>
2019-09-26 08:01:45 -0400

    bash raindrops revision

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -15,27 +15,22 @@ main () {
     local num=$1
     local output=""
 
-    if (( $num % 3 == 0 ))
+    if (( num % 3 == 0 ))
     then
         output+="Pling"
     fi
 
-    if (( $num % 5 == 0 ))
+    if (( num % 5 == 0 ))
     then
          output+="Plang"
     fi
 
-    if (( $num % 7 == 0 ))
+    if (( num % 7 == 0 ))
     then
         output+="Plong"
     fi
 
-    if [[ -z $output ]]
-    then
-        echo "$1"
-    else
-        echo $output
-    fi
+    echo ${output:-1}
 }
 
 main "$@"
