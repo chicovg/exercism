61bbc8b5a82392fa85baafc6d27e0f2d3f68f86a
Victor Guthrie <guthrievictor@gmail.com>
2019-09-26 07:45:31 -0400

    bash raindrops initial submission

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 #
 # Calculates the hamming difference between two DNA strains
 #
---
+++ bash/raindrops/raindrops.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# raindrops.sh
+#
+# Converts a number to a string, the contents of which depend on the number's factors.
+# - If the number has 3 as a factor, output 'Pling'.
+# - If the number has 5 as a factor, output 'Plang'.
+# - If the number has 7 as a factor, output 'Plong'.
+# - If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+
+set -o errexit
+set -o nounset
+
+main () {
+    local num=$1
+    local output=""
+
+    if (( $num % 3 == 0 ))
+    then
+        output+="Pling"
+    fi
+
+    if (( $num % 5 == 0 ))
+    then
+         output+="Plang"
+    fi
+
+    if (( $num % 7 == 0 ))
+    then
+        output+="Plong"
+    fi
+
+    if [[ -z $output ]]
+    then
+        echo "$1"
+    else
+        echo $output
+    fi
+}
+
+main "$@"
