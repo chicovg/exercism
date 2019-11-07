df1d1a69c074a82574457ff8d065a20968cefdba
Victor Guthrie <guthrievictor@gmail.com>
2019-11-07 09:20:44 -0500

    improved my clojure hamming solution

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,9 +1,7 @@
 (ns hamming)
 
 (defn distance [strand1 strand2]
-  (if (not= (count strand1) (count strand2))
-    nil
-    (->> (interleave strand1 strand2)
-         (partition 2)
-         (filter #(let [[a b] %] (not= a b)))
+  (when (= (count strand1) (count strand2))
+    (->> (map = strand1 strand2)
+         (filter false?)
          count)))
